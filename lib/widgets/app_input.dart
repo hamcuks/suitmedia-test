890b4b54af52fa6ff73cbf69093a323d17a68a9c
Ilham S <ivannurilham60@gmail.com>
2023-01-14 10:01:35 +0700

    refactor(form): add validator on text input

--- lib/widgets/app_input.dart
+++ lib/widgets/app_input.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class AppInputForm extends StatelessWidget {
@@ -5,15 +7,22 @@ class AppInputForm extends StatelessWidget {
     Key? key,
     required this.hint,
     required this.controller,
+    this.required = false,
   }) : super(key: key);
 
   final String hint;
   final TextEditingController controller;
+  final bool required;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
+      validator: (value) {
+        if (required && (value == null || value == '')) {
+          return 'Form harus diisi!';
+        }
+      },
       decoration: InputDecoration(
         hintText: hint,
         hintStyle: const TextStyle(color: Colors.black45),
@@ -23,6 +32,7 @@ class AppInputForm extends StatelessWidget {
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
         ),
+        errorStyle: const TextStyle(color: Colors.white),
       ),
     );
   }
